25aa1cfe12be813f8b0d8fd11aa7c07b8eb2605c
Enzo-RAG <enzogroisard@hotmail.fr>
2022-07-02 21:55:58 +0200

    [feat:header]prettier type script header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,45 @@
+import i18next, { t } from 'i18next'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Styles from '../styles/Header.module.scss'
+import { useTranslation } from 'react-i18next'
 
-
-import React from 'react';
-import {Link} from 'react-router-dom'
-import Styles from '../styles/Header.module.scss';
-
+const lngs = {
+    en: { nativeName: 'English' },
+    de: { nativeName: 'Deutsch' },
+}
 function Nav() {
+    const { t } = useTranslation()
+    return (
+        <nav className={Styles.header}>
+            <img src="./tobalogo.svg" alt="logo"></img>
+            <Link to="/" className={Styles.header__link}>
+                home
+            </Link>
+            <Link to="/motionDesign" className={Styles.header__link}>
+                motion design
+            </Link>
+            <Link to="/motionDesign" className={Styles.header__link}>
+                autre realisation
+            </Link>
+            <Link to="/motionDesign" className={Styles.header__link}>
+                qui suis-je?
+            </Link>
 
-  
-  return (
-
-    <nav className={Styles.header}>
-        <Link to="/motionDesign" className={Styles.header__link}>motion design</Link>
-        <Link to="/" className={Styles.header__link}>home</Link>
-        
-        
-    </nav>
-  );
+            <div>
+                {Object.keys(lngs).map((lng) => (
+                    <button
+                        type="submit"
+                        key={lng}
+                        onClick={() => i18next.changeLanguage(lng)}
+                        disabled={i18next.resolvedLanguage === lng}
+                    >
+                        {lngs[lng].nativeName}
+                    </button>
+                ))}
+            </div>
+        </nav>
+    )
 }
 
-
-export default(Nav)
+export default Nav
