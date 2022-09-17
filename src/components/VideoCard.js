a82561b7f08bf9e7c6c98c3148e5e144559b2340
Enzo-RAG <enzogroisard@hotmail.fr>
2022-09-17 17:45:46 +0200

    [feat:motion design page]create card

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,7 @@
 import i18next from 'i18next'
 import React, { useState } from 'react'
 import styles from '/home/enzo/projet/toba-design/src/styles/Navigations/partials/LanguageButton.module.scss'
-
+import '/home/enzo/projet/toba-design/src/styles/components/VideosCard.css'
 const lngs = {
     en: { nativeName: 'English' },
     fr: { nativeName: 'Francais' },
@@ -13,10 +13,10 @@ function LanguageButton(props) {
         <div className="flip-card">
             <div className="flip-card-inner">
                 <div className="flip-card-front">
-                    <img src="" />
+                    <img src="/home/enzo/projet/toba-design/public/toba logo insta.png" />
                 </div>
                 <div className="flip-card-back">
-                    <img />
+                    <img src="/home/enzo/projet/toba-design/public/toba logo insta.png" />
                     <h3> prejent</h3>
                     <h1>la</h1>
                 </div>
