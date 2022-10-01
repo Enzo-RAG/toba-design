5a0829021937c07a36b913358e209222a0c430f5
Enzo-RAG <enzogroisard@hotmail.fr>
2022-10-01 15:38:09 +0200

    [feat:color_master] creation of color master file and modification of scss file

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,13 +1,7 @@
-import i18next from 'i18next'
 import { Link } from 'react-router-dom'
 import React, { useState } from 'react'
-import styles from '/home/enzo/projet/toba-design/src/styles/Navigations/partials/LanguageButton.module.scss'
-import '/home/enzo/projet/toba-design/src/styles/components/VideosCard.css'
+import styles from '/home/enzo/projet/toba-design/src/styles/components/VideosCard.module.scss'
 
-const lngs = {
-    en: { nativeName: 'English' },
-    fr: { nativeName: 'Francais' },
-}
 function LanguageButton(props) {
     const [Test, setTest] = useState(props.testdata)
     const [FrontPicture, setFrontPicture] = useState(props.frontPicture)
@@ -16,15 +10,15 @@ function LanguageButton(props) {
     const [Descritption, setDescritption] = useState(props.description)
 
     return (
-        <div className="flip-card">
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                    <div className="test">
-                        <img className="flip-card-front-img" src={FrontPicture} />
+        <div className={styles[`flip-card`]}>
+            <div className={styles[`flip-card-inner`]}>
+                <div className={styles[`flip-card-front`]}>
+                    <div className={styles[`test`]}>
+                        <img className={styles[`flip-card-front-img`]} src={FrontPicture} />
                     </div>
                 </div>
-                <div className="flip-card-back">
-                    <Link to="/motionDesign/detail&id=6">
+                <div className={styles[`flip-card-back`]}>
+                    <Link className={styles[`flip-card-back-text`]} to="/motionDesign/detail&id=6">
                         <img src={MiniPicture} />
                         <h2> {Title}</h2>
                         <h3>{Descritption}</h3>
